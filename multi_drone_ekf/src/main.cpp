4f154f8f4cd48ec99c18850dac584c8fb825dbf3
Karol <karol@karol-K53SV.(none)>
2014-02-10 20:43:36 -0800

    change transform, calibration needed.

--- multi_drone_ekf/src/main.cpp
+++ multi_drone_ekf/src/main.cpp
@@ -54,30 +54,50 @@ struct Camera {
                 continue;
 
 
+            trans_x_ = tag.xMetric;
+            trans_y_ = tag.yMetric;
+            trans_z_ = tag.zMetric;
+            rot_z_ = -tag.yRot;
+            rot_y_ = -tag.xRot;
+            rot_x_ = tag.zRot;
 
-            trans_x_ += tag.yMetric;
-            trans_y_ += tag.zMetric;
-            trans_z_ += tag.xMetric;
-            rot_x_ += tag.zRot;
-            rot_y_ += -tag.yRot;
-            rot_z_ += -tag.xRot;
+
+
+            btVector3 trans_around_y(0,0,0);
+
+            btQuaternion rot_around_y;
+            rot_around_y.setEulerZYX(0,-M_PI/2,0);
+
+            tf::Transform pose_around_y;
+
+
+            pose_around_y.setOrigin(trans_around_y);
+            pose_around_y.setRotation(rot_around_y);
+
+//            trans_x_ = tag.yMetric;
+//            trans_y_ = tag.zMetric;
+//            trans_z_ = tag.xMetric;
+//            rot_z_ = tag.zRot - M_PI/2;
+//            rot_x_ = tag.yRot;
+//            rot_y_ = -tag.xRot - M_PI;
 
             counter_ ++;
 
-            if (counter_ == avg_number_)
+//            if (counter_ == avg_number_)
             {
 
-                btVector3 translation(trans_x_/avg_number_, trans_y_/avg_number_, trans_z_/avg_number_);
+                btVector3 translation(trans_x_/*/avg_number_*/, trans_y_/*/avg_number_*/, trans_z_/*/avg_number_*/);
 
 
                 btQuaternion rotation;
 
-                rotation.setEulerZYX(rot_x_/avg_number_, rot_y_/avg_number_, rot_z_/avg_number_);
+                rotation.setEulerZYX(rot_z_/*/avg_number_*/, rot_y_/*/avg_number_*/, rot_x_/*/avg_number_*/);
 
 
                 if (tag.id == marker){
                     tag_pose_.setOrigin(translation);
                     tag_pose_.setRotation(rotation);
+                    tag_pose_ = tag_pose_*pose_around_y;
                     pose_set_ = true;
                 }
             }
@@ -145,11 +165,44 @@ struct Ardrone {
 				continue;
 
 
-            btVector3 translation(tag.yMetric, tag.zMetric, tag.xMetric);
+//            btVector3 translation(tag.yMetric, tag.zMetric, tag.xMetric);
 
+//            btQuaternion rotation;
+
+//            rotation.setEulerZYX(tag.zRot, -tag.yRot, -tag.xRot);
+
+
+//            double trans_x_ = tag.yMetric;
+//            double trans_y_ = tag.zMetric;
+//            double trans_z_ = tag.xMetric;
+//            double rot_z_ = tag.zRot - M_PI/2;
+//            double rot_x_ = tag.yRot;
+//            double rot_y_ = -tag.xRot - M_PI;
+
+
+           double trans_x_ = tag.xMetric;
+           double trans_y_ = tag.yMetric;
+           double trans_z_ = tag.zMetric;
+           double rot_z_ = -tag.yRot;
+           double  rot_y_ = -tag.xRot;
+           double rot_x_ = tag.zRot;
+
+
+
+            btVector3 trans_around_y(0,0,0);
+
+            btQuaternion rot_around_y;
+            rot_around_y.setEulerZYX(0,-M_PI/2,0);
+
+            tf::Transform pose_around_y;
+
+
+            pose_around_y.setOrigin(trans_around_y);
+            pose_around_y.setRotation(rot_around_y);
+
+            btVector3 translation(trans_x_,trans_y_,trans_z_);
             btQuaternion rotation;
-
-            rotation.setEulerZYX(tag.zRot, -tag.yRot, -tag.xRot);
+            rotation.setEulerZYX(rot_z_, rot_y_,rot_x_);
 
 
             if (tag.id==marker){
@@ -157,6 +210,7 @@ struct Ardrone {
 
                 tag_pose_.setOrigin(translation);
                 tag_pose_.setRotation(rotation);
+                tag_pose_ = tag_pose_*pose_around_y;
 
 
 
@@ -299,11 +353,11 @@ int main(int argc, char **argv) {
 
 
             br.sendTransform(
-                    tf::StampedTransform(camera.tag_pose_, ros::Time::now(), "/zeta_marker",
+                    tf::StampedTransform(camera.tag_pose_.inverse(), ros::Time::now(), "/zeta_marker",
                             "/camera"));
 
             br.sendTransform(
-                    tf::StampedTransform(drone_observer.tag_pose_*camera.tag_pose_.inverse(), ros::Time::now(), "/zeta_marker",
+                    tf::StampedTransform(camera.tag_pose_.inverse()*drone_observer.tag_pose_, ros::Time::now(), "/zeta_marker",
                             "/beta_marker"));
 
         }
